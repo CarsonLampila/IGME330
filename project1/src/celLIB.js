7d8622ab79fd9f1a975dd7625d09e629a2485c21
Carson Lampila <43243165+CarsonLampila@users.noreply.github.com>
2020-09-17 21:00:34 -0400

    Done except Documentation

--- project1/src/celLIB.js
+++ project1/src/celLIB.js
@@ -4,24 +4,42 @@ console.log("celLIB.js loaded");
 (function(){
 	let celLIB = 
 	{
-		// Return Random Color
-		getRandomColor()
-		{
-			const getByte = _ => 55 + Math.round(Math.random() * 200);		
-			return `rgba(${getByte()},${getByte()},${getByte()},.8)`;
-		},
-	
 		// Reurn Random Int
 		getRandomInt(min, max) 
 		{
 			return Math.floor(Math.random() * (max - min + 1)) + min;
 		},
 		
+		// Return Random Color
+		getRandomColor()
+		{
+			const getByte = _ => 55 + Math.round(Math.random() * 200);		
+			return `rgba(${getByte()},${getByte()},${getByte()},.8)`;
+		},
+		
+		// Increment a number
+		increment(input, direction, change){
+			switch(direction){
+				// None
+				case 0:
+					break;
+				// Inc
+				case 1:
+					input += change;
+					break;
+				// Dec
+				case 2:
+					input -= change;
+					break;
+			}
+			return input;
+		},
+		
 		// Convert to Degrees
 		dtr(degrees){
 			return degrees * (Math.PI/180);
 		},
-		
+			
 		// Clear Screen
 		cls(ctx, canvasStartX, canvasStartY){
 			ctx.clearRect(canvasStartX, canvasStartY, canvas.width + canvasStartX, canvas.height + canvasStartY);
@@ -30,34 +48,78 @@ console.log("celLIB.js loaded");
 		},
 		
 		// Draw Circles
-		drawCircle(ctx, cX, cY, cSize, fillStyle="black", lineWidth=0)
-		{
+		drawCircle(ctx, x, y, size, fillStyle="black", lineWidth=0, strokeStyle="black"){
 			ctx.save();
 			ctx.fillStyle = fillStyle;
 			ctx.beginPath();
-			ctx.arc(cX,cY,cSize,0,Math.PI * 2, false);
+			ctx.arc(x,y,size,0,Math.PI * 2, false);
 			ctx.closePath();
 			ctx.lineWidth = lineWidth;
+			ctx.strokeStyle = strokeStyle;
+			ctx.stroke();
 			ctx.fill();
 			ctx.restore();
 		},
 		
 		// Draw Rectangle
-		drawRectangle(ctx, cX, cY, width, height, fillStyle="black", lineWidth=0, strokeStyle="black")
-		{
-			ctx.fillStyle = fillStyle; 
+		drawRectangle(ctx, x, y, width, height, fillStyle="black", lineWidth=0, strokeStyle="black"){
 			ctx.save();
+			ctx.fillStyle = fillStyle; 
 			ctx.beginPath();
-			ctx.rect(cX,cY,width,height);
+			ctx.rect(x,y,width,height);
 			ctx.closePath();
+			ctx.lineWidth = lineWidth;
+			ctx.strokeStyle = strokeStyle;
+			ctx.stroke();
 			ctx.fill();
-			
-			if(lineWidth > 0)
-			{
-				ctx.lineWidth = lineWidth;
-				ctx.strokeStyle = strokeStyle;
-				ctx.stroke();
-			}
+			ctx.restore();
+		},
+		
+		// Draw Triangle
+		drawTriangle(ctx, x, y, size, fillStyle="black", lineWidth=0, strokeStyle="black"){
+			ctx.save();
+			ctx.fillStyle = fillStyle;
+ 			ctx.beginPath();
+ 			ctx.moveTo(x, y);
+ 			ctx.lineTo(x + size/2, y - size);
+  			ctx.lineTo(x - size/2, y - size);
+ 			ctx.closePath();
+			ctx.lineWidth = lineWidth;
+			ctx.strokeStyle = strokeStyle;
+ 			ctx.stroke();
+			ctx.fill();
+			ctx.restore();
+		},
+		
+		// Draw Diamond
+		drawDiamond(ctx, x, y, size, fillStyle="black", lineWidth=0, strokeStyle="black"){
+			ctx.save();
+			ctx.fillStyle = fillStyle;
+ 			ctx.beginPath();
+ 			ctx.moveTo(x, y);
+ 			ctx.lineTo(x + size/2, y - (size * (2/3)));
+ 			ctx.lineTo(x, y - (size * (3/2)));
+  			ctx.lineTo(x - size/2, y - (size * (2/3)));
+ 			ctx.closePath();
+			ctx.lineWidth = lineWidth;
+			ctx.strokeStyle = strokeStyle;
+ 			ctx.stroke();
+			ctx.fill();
+			ctx.restore();
+		},
+		
+		// Draw X
+		drawX(ctx, x, y, size, lineWidth=1, strokeStyle="black"){
+			ctx.save();
+ 			ctx.beginPath();
+ 			ctx.moveTo(x - size/2, y + size/2);
+ 			ctx.lineTo(x + size/2, y - size/2);
+  			ctx.moveTo(x + size/2, y + size/2);
+ 			ctx.lineTo(x - size/2, y - size/2);
+ 			ctx.closePath();
+			ctx.lineWidth = lineWidth;
+			ctx.strokeStyle = strokeStyle;
+ 			ctx.stroke();
 			ctx.restore();
 		},
 		
--- project1/src/index.js
+++ project1/src/index.js
@@ -5,173 +5,403 @@ console.log("index.js loaded");
 	const canvasWidth = 1000, canvasHeight = 700;
 	const canvasStartX = 0, canvasStartY = 0;
 	let ctx;
+	let animate;
 	let mouseX = -1, mouseY = -1;
 	let x = 0, y = 0;
 	let n = 0;
+	let stop = true, reverse = false;
+	let shape = 0;
 	let colorStart = 0, colorEnd = -1, colorChange = 0;
-	let radius = 2;
-	let spaceStart = 10, spaceChange = 10;
+	let radius = 2.5, sizeChange = 0;
+	let space = 10, padChange = 0;
+	let change = .005;
 	let divergence = 137.5;
 	let fps = 60;
-	let stop = true, reverse = false;
-
 	
-	let index = 
-	{
-		// On window load
-		init(){
-			ctx = canvas.getContext("2d");
-			canvas.width = canvasWidth;
-			canvas.height = canvasHeight;
-			ctx.fillRect(canvasStartX,canvasStartY,canvasWidth + canvasStartX,canvasHeight + canvasStartY);
-			index.setupUI();
-		},
+	
+	window.onload = init;
 		
-		// Setup UI
-		setupUI()
-		{		
-			// Draw
-			canvas.onclick = index.canvasClicked;
-			
-			// Button Controls
-			document.querySelector("#btnStart").onclick = function(e){
-				stop = false;
-				index.loop();
-			}; 	
-			document.querySelector("#btnPause").onclick = function(e){
-				stop = true;
-			}; 	
-			document.querySelector("#btnReverse").onclick = function(e){
-				reverse = !reverse;
-				divergence = -divergence;	
-			};
-			document.querySelector("#btnClear").onclick = function(e){
-				celLIB.cls(ctx, canvasStartX, canvasStartY);
-				stop = true;
-				n = 0;
-			}; 		
-			document.querySelector("#btnExport").onclick = celLIB.doExport;
-			
-			// Color Controls
-			document.querySelector("#sRed").checked = true;
-			document.querySelector("#sRed").onclick = function(e){ index.colorSet(0, colorEnd); };
-			document.querySelector("#sOrange").onclick = function(e){ index.colorSet(50, colorEnd); };
-			document.querySelector("#sYellow").onclick =  function(e){index.colorSet(100, colorEnd); };
-			document.querySelector("#sGreen").onclick = function(e){ index.colorSet(200, colorEnd); };
-			document.querySelector("#sBlue").onclick = function(e){ index.colorSet(350, colorEnd); }; 
-			document.querySelector("#sPurple").onclick = function(e){ index.colorSet(500, colorEnd); };
-			
-			document.querySelector("#eCycle").checked = true;
-			document.querySelector("#eRed").onclick = function(e){ index.colorSet(colorStart, 0); };
-			document.querySelector("#eOrange").onclick = function(e){ index.colorSet(colorStart, 50); };
-			document.querySelector("#eYellow").onclick =  function(e){index.colorSet(colorStart, 100); };
-			document.querySelector("#eGreen").onclick = function(e){ index.colorSet(colorStart, 200); };
-			document.querySelector("#eBlue").onclick = function(e){ index.colorSet(colorStart, 350); };
-			document.querySelector("#ePurple").onclick = function(e){ index.colorSet(colorStart, 500); };
-			document.querySelector("#eCycle").onclick = function(e){ index.colorSet(colorStart, -1); };
-			
-			// Petal Controls
-			document.querySelector("#petalSize").oninput = function(e){ radius = document.querySelector("#petalSize").value / 10; };
-			
-			// Space Controls
-			document.querySelector("#padding").oninput = function(e){ 
-				spaceStart = document.querySelector("#padding").value; 
-				spaceChange = document.querySelector("#padding").value; 
-			};
-			
-			// FPS Controls
-			document.querySelector("#fps4").checked = true;
-			document.querySelector("#fps1").onclick = function(e){ fps = 15; };
-			document.querySelector("#fps2").onclick = function(e){ fps = 30; };
-			document.querySelector("#fps3").onclick = function(e){ fps = 45; };
-			document.querySelector("#fps4").onclick = function(e){ fps = 60; };
-			document.querySelector("#fps5").onclick = function(e){ fps = 75; };
-			document.querySelector("#fps6").onclick = function(e){ fps = 90; };
-			document.querySelector("#fps7").onclick = function(e){ fps = 105; };
-			document.querySelector("#fps8").onclick = function(e){ fps = 120; };
-				
-			// Degree Controls
-			document.querySelector("#degree4").checked = true;
-			document.querySelector("#degree1").onclick =  function(e){index.degreeTransition(100.0); };
-			document.querySelector("#degree2").onclick = function(e){ index.degreeTransition(129.1); };
-			document.querySelector("#degree3").onclick = function(e){ index.degreeTransition(-137.1); };
-			document.querySelector("#degree4").onclick = function(e){ index.degreeTransition(137.5); };
-			document.querySelector("#degree5").onclick = function(e){ index.degreeTransition(-137.9); };
-			document.querySelector("#degree6").onclick = function(e){ index.degreeTransition(-138.1); };
-			document.querySelector("#degree7").onclick = function(e){ index.degreeTransition(200.0); };
-		},
+	// On window load
+	function init(){
+		ctx = canvas.getContext("2d");
+		canvas.width = canvasWidth;
+		canvas.height = canvasHeight;
+		ctx.fillRect(canvasStartX,canvasStartY,canvasWidth + canvasStartX,canvasHeight + canvasStartY);
+		setupUI();
+	}
+		
+	// Setup UI
+	function setupUI()
+	{		
+		// Draw
+		canvas.onclick = canvasClicked;
 
-		// Draw Fireworks
-		loop()
-		{
-			// Pause
-			if (stop) return;
-			
-			// each frame draw a new dot
-			setTimeout(index.loop,1000/fps);
-
-			// `a` is the angle
-			// `n` is the distance from start
-			// `r` is the radius
-			// `space` is the "padding/spacing" between the dots
-			let a = n * celLIB.dtr(divergence);
-			let r = spaceChange * Math.sqrt(n);
-		
-			// Calculate
-			let x = r * Math.cos(a) + mouseX;
-			let y = r * Math.sin(a) + mouseY;
-			
-			colorChange = celLIB.colorTransition(colorChange, colorStart, colorEnd);	
-			let color = `hsl(${colorChange/2 % 361},100%,50%)`;	
-		
-			// Draw
-			celLIB.drawCircle(ctx,x,y,radius,color);
-				
-			// Alternate	
-			n++;
-			spaceChange-=.005;
-		}, 
-		
-		// Start firework where click
-		canvasClicked(e){
-			// Find x and y
-			let rect = e.target.getBoundingClientRect();
-			mouseX = e.clientX - rect.x;
-			mouseY = e.clientY - rect.y;
-			
-			// Reset
-			n = 0;
-			spaceChange = spaceStart;
-			colorChange = colorStart;
+		// Button Controls
+		document.querySelector("#btnPause").onclick = function(e){
+			stop = true;
+		}; 	
+		document.querySelector("#btnResume").onclick = function(e){
 			stop = false;
-			index.loop();
-		},
-		
-		// Set Color
-		colorSet(cStart, cEnd){
+			loop();
+		}; 	
+		document.querySelector("#btnRandom").onclick = randomize;
+		document.querySelector("#btnReverse").onclick = function(e){
+			reverse = !reverse;
+			divergence = -divergence;	
+		};
+		document.querySelector("#btnClear").onclick = function(e){
+			celLIB.cls(ctx, canvasStartX, canvasStartY);
 			stop = true;
 			n = 0;
-			spaceChange = spaceStart;
-			colorStart = cStart;
-			colorEnd = cEnd;
-			colorChange = colorStart;
-			
-		},
+		}; 		
+		document.querySelector("#btnExport").onclick = celLIB.doExport;
 		
-		// Transition Degree
-		degreeTransition(dStart){
-			divergence = dStart;
-			if(reverse)
-				divergence = -divergence; 
-		}
-	};
-	
-	if (window)
-	{
-		window["index"] = index;
+		// Shape Controls
+		document.querySelector("#shape1").checked = true;
+		document.querySelector("#shape1").onclick = function(e){ shape = 0; };
+		document.querySelector("#shape2").onclick = function(e){ shape = 1; };
+		document.querySelector("#shape3").onclick = function(e){ shape = 2; };
+		document.querySelector("#shape4").onclick = function(e){ shape = 3; };
+		document.querySelector("#shape5").onclick = function(e){ shape = 4; }; 
+			
+		// Color Start Controls
+		document.querySelector("#sRed").checked = true;
+		document.querySelector("#sRed").onclick = function(e){ colorSet(0, colorEnd); };
+		document.querySelector("#sOrange").onclick = function(e){ colorSet(50, colorEnd); };
+		document.querySelector("#sYellow").onclick = function(e){ colorSet(100, colorEnd); };
+		document.querySelector("#sGreen").onclick = function(e){ colorSet(200, colorEnd); };
+		document.querySelector("#sBlue").onclick = function(e){ colorSet(350, colorEnd); }; 
+		document.querySelector("#sPurple").onclick = function(e){ colorSet(500, colorEnd); };
+			
+		// Color End Controls
+		document.querySelector("#eCycle").checked = true;
+		document.querySelector("#eRed").onclick = function(e){ colorSet(colorStart, 0); };
+		document.querySelector("#eOrange").onclick = function(e){ colorSet(colorStart, 50); };
+		document.querySelector("#eYellow").onclick =  function(e){ colorSet(colorStart, 100); };
+		document.querySelector("#eGreen").onclick = function(e){ colorSet(colorStart, 200); };
+		document.querySelector("#eBlue").onclick = function(e){ colorSet(colorStart, 350); };
+		document.querySelector("#ePurple").onclick = function(e){ colorSet(colorStart, 500); };
+		document.querySelector("#eCycle").onclick = function(e){ colorSet(colorStart, -1); };
+			
+		// Petal Controls
+		document.querySelector("#petalSize").oninput = function(e){ radius = document.querySelector("#petalSize").value / 10; };
+		document.querySelector("#sNone").checked = true;
+		document.querySelector("#sNone").onclick = function(e){ sizeChange = 0; };
+		document.querySelector("#sInc").onclick = function(e){ sizeChange = 1; };
+		document.querySelector("#sDec").onclick = function(e){ sizeChange = 2; };
+			
+		// Space Controls
+		document.querySelector("#padding").oninput = function(e){ space = document.querySelector("#padding").value / 10; };
+		document.querySelector("#pNone").checked = true;
+		document.querySelector("#pNone").onclick = function(e){ padChange = 0; };
+		document.querySelector("#pInc").onclick = function(e){ padChange = 1; };
+		document.querySelector("#pDec").onclick = function(e){ padChange = 2; };
+		
+		// Degree Controls
+		document.querySelector("#degree4").checked = true;
+		document.querySelector("#degree1").onclick = function(e){ degreeTransition(100.0); };
+		document.querySelector("#degree2").onclick = function(e){ degreeTransition(129.1); };
+		document.querySelector("#degree3").onclick = function(e){ degreeTransition(-137.1); };
+		document.querySelector("#degree4").onclick = function(e){ degreeTransition(137.5); };
+		document.querySelector("#degree5").onclick = function(e){ degreeTransition(-137.9); };
+		document.querySelector("#degree6").onclick = function(e){ degreeTransition(-138.1); };
+		document.querySelector("#degree7").onclick = function(e){ degreeTransition(200.0); };
+			
+		// FPS Controls
+		document.querySelector("#fps4").checked = true;
+		document.querySelector("#fps1").onclick = function(e){ fps = 15; };
+		document.querySelector("#fps2").onclick = function(e){ fps = 30; };
+		document.querySelector("#fps3").onclick = function(e){ fps = 45; };
+		document.querySelector("#fps4").onclick = function(e){ fps = 60; };
+		document.querySelector("#fps5").onclick = function(e){ fps = 75; };
+		document.querySelector("#fps6").onclick = function(e){ fps = 90; };
+		document.querySelector("#fps7").onclick = function(e){ fps = 105; };
+		document.querySelector("#fps8").onclick = function(e){ fps = 120; };
 	}
-	else
+
+	// Draw Fireworks
+	function loop()
 	{
-		throw "'window' is not defined!";
+		// Pause
+		if (stop) return;
+		
+		// each frame draw a new dot
+		animate = setTimeout(loop,1000/fps);
+
+		// `a` is the angle
+		// `n` is the distance from start
+		// `r` is the radius
+		// `space` is the "padding/spacing" between the dots
+		let a = n * celLIB.dtr(divergence);
+		let r = space * Math.sqrt(n);
+		
+		// Calculate
+		x = r * Math.cos(a) + mouseX;
+		y = r * Math.sin(a) + mouseY;
+		
+		radius = celLIB.increment(radius, sizeChange, change);
+			
+		colorChange = celLIB.colorTransition(colorChange, colorStart, colorEnd);	
+		let color = `hsl(${colorChange/2 % 361},100%,50%)`;	
+		
+		// Draw
+		draw(x, y, color);
+				
+		// Alternate	
+		space = celLIB.increment(space, padChange, change);
+		n++;
+	}
+		
+	// Start firework where click
+	function canvasClicked(e){
+		// Find x and y
+		let rect = e.target.getBoundingClientRect();
+		mouseX = e.clientX - rect.x;
+		mouseY = e.clientY - rect.y;
+		
+		// Reset
+		clearTimeout(animate);
+		n = 0;
+		colorChange = colorStart;
+		stop = false;
+		loop();
+	}
+	
+	// Draw Shapes
+	function draw(x, y, color){
+		switch(shape){
+			// Circle
+			case 0:
+				celLIB.drawCircle(ctx,x,y,radius,color);
+				break;
+			// Square
+			case 1:
+				celLIB.drawRectangle(ctx,x,y,radius*2,radius*2,color);
+				break;	
+			// Triangle
+			case 2:
+				celLIB.drawTriangle(ctx,x,y,radius* 2,color);
+				break;
+			// Diamond
+			case 3:
+				celLIB.drawDiamond(ctx,x,y,radius* 2,color);
+				break;
+			// X
+			case 4:
+				celLIB.drawX(ctx,x,y,radius* 2,radius,color);
+				break;
+		}
+	}
+		
+	// Set Color
+	function colorSet(cStart, cEnd){
+		stop = true;
+		n = 0;
+		colorStart = cStart;
+		colorEnd = cEnd;
+		colorChange = colorStart;	
+	}
+		
+	// Transition Degree
+	function degreeTransition(dStart){
+		divergence = dStart;
+		if(reverse)
+			divergence = -divergence; 
+	}
+	
+	// Randomize Selection
+	function randomize(){
+		// Shape
+		let sh = celLIB.getRandomInt(0,4);
+		switch(sh){
+			case 0:
+				document.querySelector("#shape1").checked = true;
+				shape = sh;
+				break;
+			case 1:
+				document.querySelector("#shape2").checked = true;
+				shape = sh;
+				break;
+			case 2:
+				document.querySelector("#shape3").checked = true;
+				shape = sh;
+				break;
+			case 3:
+				document.querySelector("#shape4").checked = true;
+				shape = sh;
+				break;
+			case 4:
+				document.querySelector("#shape5").checked = true;
+				shape = sh;
+				break;						
+		}
+		
+		// Start Color
+		let sc = celLIB.getRandomInt(0,5);
+		switch(sc){
+			case 0:
+				document.querySelector("#sRed").checked = true;
+				colorSet(0, colorEnd);
+				break;
+			case 1:
+				document.querySelector("#sOrange").checked = true;
+				colorSet(50, colorEnd);
+				break;
+			case 2:
+				document.querySelector("#sYellow").checked = true;
+				colorSet(100, colorEnd);
+				break;
+			case 3:
+				document.querySelector("#sGreen").checked = true;
+				colorSet(200, colorEnd);
+				break;
+			case 4:
+				document.querySelector("#sBlue").checked = true;
+				colorSet(350, colorEnd);
+				break;
+			case 5:
+				document.querySelector("#sPurple").checked = true;
+				colorSet(500, colorEnd);
+				break;							
+		}
+			
+		// End Color
+		let ec = celLIB.getRandomInt(0,6);	
+		switch(ec) {
+			case 0:
+				document.querySelector("#eRed").checked = true;
+				colorSet(colorStart, 0);
+				break;
+			case 1:
+				document.querySelector("#eOrange").checked = true;
+				colorSet(colorStart, 50);
+				break;
+			case 2:
+				document.querySelector("#eYellow").checked = true;
+				colorSet(colorStart, 100);
+				break;
+			case 3:
+				document.querySelector("#eGreen").checked = true;
+				colorSet(colorStart, 200);
+				break;
+			case 4:
+				document.querySelector("#eBlue").checked = true;
+				colorSet(colorStart, 350);
+				break;
+			case 5:
+				document.querySelector("#ePurple").checked = true;
+				colorSet(colorStart, 500);
+				break;		
+			case 6:
+				document.querySelector("#eCycle").checked = true;
+				colorSet(colorStart, -1);
+				break;						
+		}
+			
+		// Size
+		let s = celLIB.getRandomInt(1,50);
+		document.querySelector("#petalSize").value = s;
+		radius = s / 10;
+		let cs = celLIB.getRandomInt(0,2);
+		switch(cs){
+			case 0:
+				document.querySelector("#sNone").checked = true;
+				break;
+			case 1:
+				document.querySelector("#sInc").checked = true;
+				break;
+			case 2:
+				document.querySelector("#sDec").checked = true;
+				break;						
+		}
+		sizeChange = cs;
+	
+		// Padding
+		let p = celLIB.getRandomInt(1,200);
+		document.querySelector("#padding").value = p; 
+		space = p / 10;
+		let cp = celLIB.getRandomInt(0,2);
+		switch(cp){
+			case 0:
+				document.querySelector("#pNone").checked = true;
+				break;
+			case 1:
+				document.querySelector("#pInc").checked = true;
+				break;
+			case 2:
+				document.querySelector("#pDec").checked = true;
+				break;						
+		}
+		padChange = cp;
+			
+		// Degree
+		let d = celLIB.getRandomInt(0,6);
+		switch(d) {
+			case 0:
+				document.querySelector("#degree1").checked = true;
+				degreeTransition(100.0);
+				break;
+			case 1:
+				document.querySelector("#degree2").checked = true;
+				degreeTransition(129.1);
+				break;
+			case 2:
+				document.querySelector("#degree3").checked = true;
+				degreeTransition(-137.1);
+				break;
+			case 3:
+				document.querySelector("#degree4").checked = true;
+				degreeTransition(137.5);
+				break;
+			case 4:
+				document.querySelector("#degree5").checked = true;
+				degreeTransition(-137.9);
+				break;
+			case 5:
+				document.querySelector("#degree6").checked = true;
+				degreeTransition(-138.1);
+				break;		
+			case 6:
+				document.querySelector("#degree7").checked = true;
+				degreeTransition(200.0);
+				break;						
+		}
+			
+		// FPS
+		let f = celLIB.getRandomInt(0,7);
+		switch(f) {
+			case 0:
+				document.querySelector("#fps1").checked = true;
+				fps = 15;
+				break;
+			case 1:
+				document.querySelector("#fps2").checked = true;
+				fps = 30;
+				break;
+			case 2:
+				document.querySelector("#fps3").checked = true;
+				fps = 45;
+				break;
+			case 3:
+				document.querySelector("#fps4").checked = true;
+				fps = 60;
+				break;
+			case 4:
+				document.querySelector("#fps5").checked = true;
+				fps = 75;
+				break;
+			case 5:
+				document.querySelector("#fps6").checked = true;
+				fps = 90;
+				break;		
+			case 6:
+				document.querySelector("#fps7").checked = true;
+				fps = 105;
+				break;		
+			case 7:
+				document.querySelector("#fps8").checked = true;
+				fps = 120;
+				break;								
+		}
 	}
 })();
